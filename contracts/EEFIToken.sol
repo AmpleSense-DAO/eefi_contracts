762c17f9087e45b3cdb93515b1f1c651b000fe48
davoice-321 <74732859+davoice-321@users.noreply.github.com>
2023-03-27 13:49:03 -0400

    Update EEFIToken.sol

--- contracts/EEFIToken.sol
+++ contracts/EEFIToken.sol
@@ -4,11 +4,11 @@ pragma solidity 0.7.6;
 import '@balancer-labs/v2-solidity-utils/contracts/openzeppelin/Ownable.sol';
 import '@balancer-labs/v2-solidity-utils/contracts/openzeppelin/ERC20Burnable.sol';
 
-//Note: Only the Amplesense vault contract (ElasticVault.sol) is authorized to mint or burn EEFI 
+//Note: Only the Elastic Vault contract (ElasticVault.sol) is authorized to mint or burn EEFI 
 
 contract EEFIToken is ERC20Burnable, Ownable {
     constructor() 
-    ERC20("Amplesense Elastic Finance token", "EEFI")
+    ERC20("Elastic Finance Token", "EEFI")
     Ownable() {
     }
 
